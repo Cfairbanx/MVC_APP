e753ec4839a862cb6f2fce70940644b637326512
Cfairbanx <69484576+Cfairbanx@users.noreply.github.com>
2020-12-30 20:16:48 -0800

    Final Challenges
    AppDateTime and CodeFirstSample

---
+++ CodeFirstSample/CodeFirstSample/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            using (var db = new SchoolContext())
+            {
+                Console.Write("Enter a Student's name.");
+                var name = Console.ReadLine();
+
+                var stud = new Student { StudentName = name };
+                db.Students.Add(stud);
+                db.SaveChanges();
+
+                Console.Write("Student saved successfully!");
+
+                var query = from b in db.Students
+                            orderby b.StudentName
+                            select b;
+
+            Console.WriteLine("Students in the database: ");
+            foreach (var item in query)
+            {
+                Console.WriteLine(item.StudentName);
+            }
+            Console.WriteLine("Press any key to exit...");
+            Console.ReadKey();
+            }
+        }      
+    }
+
+    public class Student
+    {
+        public int StudentID { get; set; }
+        public string StudentName { get; set; }
+        public DateTime? DateOfBirth { get; set; }
+        public byte[] Photo { get; set; }
+        public decimal Height { get; set; }
+        public float Weight { get; set; }
+
+        public Grade Grade { get; set; }
+    }
+
+    public class Grade
+    {
+        public int GradeId { get; set; }
+        public string GradeName { get; set; }
+        public string Section { get; set; }
+
+        public ICollection<Student> Students { get; set; }
+    }
+
+    public class SchoolContext : DbContext
+    {
+        public DbSet<Student> Students { get; set; }
+        public DbSet<Grade> Grades { get; set; }
+    }
+
+
---
+++ ScoresCore/ScoresCore/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace ScoresCore
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter in your first name.");
+            string date = DateTime.Today.ToShortDateString();
+            string uName = Console.ReadLine();
+            string msg = $"\nWelcome back {uName}. Today is {date}.";
+            Console.WriteLine(msg);
+
+            string path = @"C:\Users\ccfai\OneDrive\Desktop\MVCAPP\MVC_APP\ScoresCore\ScoresCore\studentScores.txt";
+            string[] lines = System.IO.File.ReadAllLines(path);
+
+            double tScore = 0.0;
+
+            Console.WriteLine("\nStudent Scores: \n");
+            foreach(string line in lines)
+            {
+                Console.Write("\n" + line);
+                double score = Convert.ToDouble(line);
+                tScore += score;
+            }
+
+            double avgScore = tScore / lines.Length;
+            Console.WriteLine("\nTotal of " + lines.Length + " student scores. \tAverage score: " + avgScore);
+            Console.WriteLine("\n\nPress any key to exit.");
+            Console.ReadKey();
+        }
+    }
+}
